4bb4bf3010949d028fbd06485cc0cfd445b80933
Darcy Murphy <darcy.murphy@merrillcorp.com>
2018-01-24 10:24:04 -0600

    Configured install script

--- install.js
+++ install.js
@@ -4,9 +4,31 @@ var conditionallySymlinkSync = require('./conditionallySymlinkSync.js')
 var log = console.log
 var home = process.env.HOME
 
+var whatGoesWhere = [{
+    what: '/src/.gitconfig',
+    where: '/.gitconfig'
+}, {
+    what: '/src/.zshrc',
+    where: '/.zshrc'
+}, {
+    what: '/src/zsh',
+    where: '/.zsh',
+    type: 'dir'
+}, {
+    what: '/src/.vimrc',
+    where: '/.vimrc'
+}, {
+    what: '/src/subl/Preferences.sublime-settings',
+    where: '/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings'
+}, {
+    what: '/src/subl/snippets',
+    where: '/Library/Application Support/Sublime Text 3/Packages/User/snippets',
+    type: 'dir'
+}]
+
 // .gitconfig
-log('\nLINKING: .gitconfig')
-conditionallySymlinkSync( __dirname + '/src/.gitconfig', home + '/.gitconfig')
+// log('\nLINKING: .gitconfig')
+// conditionallySymlinkSync( __dirname + '/src/.gitconfig', home + '/.gitconfig')
 
 // .jshintrc
 // log('\nLINKING: .jshintrc')
@@ -21,16 +43,16 @@ conditionallySymlinkSync( __dirname + '/src/.gitconfig', home + '/.gitconfig')
 // conditionallySymlinkSync( __dirname + '/src/.hyper.js', home + '/.hyper.js')
 
 // .vimrc
-log('\nLINKING: .vimrc')
-conditionallySymlinkSync( __dirname + '/src/.vimrc', home + '/.vimrc' )
+// log('\nLINKING: .vimrc')
+// conditionallySymlinkSync( __dirname + '/src/.vimrc', home + '/.vimrc' )
 
 // .zshrc
-log('\nLINKING: .zshrc')
-conditionallySymlinkSync( __dirname + '/src/.zshrc', home + '/.zshrc' )
+// log('\nLINKING: .zshrc')
+// conditionallySymlinkSync( __dirname + '/src/.zshrc', home + '/.zshrc' )
 
 // src/zsh
-log('\nLINKING: src/zsh')
-conditionallySymlinkSync( __dirname + '/src/zsh', home + '/.zsh', 'dir' )
+// log('\nLINKING: src/zsh')
+// conditionallySymlinkSync( __dirname + '/src/zsh', home + '/.zsh', 'dir' )
 
 // ansible
 // log('\nLINKING: .ansible.cfg')
@@ -43,5 +65,11 @@ conditionallySymlinkSync( __dirname + '/src/zsh', home + '/.zsh', 'dir' )
 // preferences
 // log('\nLINKING: src/subl/Preferences.sublime-settings')
 // conditionallySymlinkSync( __dirname + '/src/subl/Preferences.sublime-settings', home + '/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings' )
+
+whatGoesWhere.forEach(function(the) {
+    log('\nLINKING: ' + the.what)
+    conditionallySymlinkSync(__dirname+the.what, home+the.where, the.type)
+})
+
 
 log('\n-- DONE\n')
